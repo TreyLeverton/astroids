15037d62443765e80c9a46814ed5342da68dacbd
SaggyFlapJax <treyleverton@yahoo.com>
2025-04-16 13:46:35 -0500

    Changed main file to fix a bug, added to constants file and added player file

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from readline import redisplay
-from textwrap import fill
 import pygame
 from constants import *
 
@@ -10,17 +8,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
 
     while True:
         for event in pygame.event.get():
-            dt = pygame.clock.tick(60) / 1000
             if event.type == pygame.QUIT:
                 return 
-        fill("black")
+            
+        dt = clock.tick(60) / 1000
+        fill("black") # type: ignore
         pygame.display.flip()
-
-
+        
 if __name__ == "__main__":
     main()
