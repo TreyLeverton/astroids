37cde2d178c715281d8c8a45a8fa2c242426b44d
SaggyFlapJax <treyleverton@yahoo.com>
2025-04-16 16:22:27 -0500

    added move feature

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
-def Player(CircleShape):
+class Player(CircleShape):
     def __init__(self, x, y):
-        super.__init__(x, y, PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0 
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     # in the player class
     def triangle(self):
@@ -14,3 +18,19 @@ def Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
