ae389e90ac8ecbe60fae95d6246d94dce20086ef
SaggyFlapJax <treyleverton@yahoo.com>
2025-04-17 10:58:27 -0500

    more bug tests

--- main.py
+++ main.py
@@ -13,10 +13,6 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-
-    clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -26,6 +22,13 @@ def main():
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable)
 
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
     asteroid_field = AsteroidField()
 
     while True:
@@ -43,7 +46,9 @@ def main():
             
         screen.fill("black")
 
+        player.draw(screen)
         drawable.draw(screen)
+        
         dt = clock.tick(60) / 1000
         updatable.update(dt)
 
