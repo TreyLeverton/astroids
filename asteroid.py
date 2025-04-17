143f46f5dcfb90ff74e21ae4a9108aa429da9950
SaggyFlapJax <treyleverton@yahoo.com>
2025-04-17 10:22:09 -0500

    attempted bug fix in asteroid

--- asteroid.py
+++ asteroid.py
@@ -5,9 +5,13 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
+        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, (255, 255, 255), (radius, radius), radius)
+
     def draw(self, screen):
         pygame.draw.circle(screen, "white", (self.x, self.y), self.radius, 2)
-    
+        self.rect = self.image.get_rect(center=(x, y))
+        
     def update(self, dt):
         movement = self.velocity * dt
         self.x = movement.x
